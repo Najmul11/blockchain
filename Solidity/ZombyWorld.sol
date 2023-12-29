81011af3b8103de94c4330f604bfcee6fdbc3584
Najmul <najmulbinnurul@gmail.com>
2023-12-29 14:19:39 +0600

    solidity-zombieworld

---
+++ Solidity/ZombyWorld.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity ^0.8.22;
+
+contract ZombieFactory {
+
+    // declare our event here
+    event NewZombie(uint zombieId,string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // and fire it here
+        emit NewZombie(allZombies.length-1, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+     function getAllZombies() public view returns (Zombie[] memory) {
+        return allZombies;
+    }
+
+}
